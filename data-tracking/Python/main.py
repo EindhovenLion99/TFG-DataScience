67e33ad48038283624473ec5a2495f5a0a2277b7
Noah <alu0101134956@ull.edu.es>
2022-05-26 16:42:16 +0100

    Clip

--- data-tracking/Python/main.py
+++ data-tracking/Python/main.py
@@ -8,18 +8,17 @@ import Statistics as getstats
 # %% -- Lectura de los datos
 home, away, events = getdata.read_match_data(2)
 
-
-# %%
-getstats.plotTeamStats(events, 'SHOT')
-
-# getstats.plotTeamPosesion(events)
-
 # %%
 goals = events.loc[events['Subtype'].str.contains('TARGET-GOAL', na=False)]
+
+# %%
+fig, ax = getplot.plot_field()
+ax.plot(home['home_10_x'].iloc[:1500], home['home_10_y'].iloc[:1500], 'r', markersize = 1)
+
+# %%
 START_FRAME = events.loc[goals.index[0]]['Start Frame'] - 25 * 10
 print(START_FRAME)
 
 # %%
 END_FRAME = START_FRAME + 25 * 60 * 0.5
 getplot.clip(home.loc[START_FRAME:END_FRAME], away.loc[START_FRAME:END_FRAME], path='.', PlayerMarkerSize=11)
-# %%
