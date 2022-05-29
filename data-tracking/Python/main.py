6f25f996631d920a061862fd1280e8bb242e4c45
Noah <alu0101134956@ull.edu.es>
2022-05-29 19:59:26 +0100

    Cambio main

--- data-tracking/Python/main.py
+++ data-tracking/Python/main.py
@@ -7,18 +7,19 @@ import Statistics as getstats
 
 # %% -- Lectura de los datos
 home, away, events = getdata.read_match_data(2)
+# %%
+getplot.plot_frame(home.iloc[51], away.iloc[51], annotate=True)
 
 # %%
 goals = events.loc[events['Subtype'].str.contains('TARGET-GOAL', na=False)]
-
-# %%
-fig, ax = getplot.plot_field()
-ax.plot(home['home_10_x'].iloc[:1500], home['home_10_y'].iloc[:1500], 'r', markersize = 1)
-
-# %%
 START_FRAME = events.loc[goals.index[0]]['Start Frame'] - 25 * 10
-print(START_FRAME)
-
-# %%
 END_FRAME = START_FRAME + 25 * 60 * 0.5
 getplot.clip(home.loc[START_FRAME:END_FRAME], away.loc[START_FRAME:END_FRAME], path='.', PlayerMarkerSize=11)
+
+
+# %%
+getplot.plot_minutes_played(home, 'home')
+
+
+# %%
+getplot.plot_minutes_played(away, 'away')
