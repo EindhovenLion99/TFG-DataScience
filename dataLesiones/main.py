5ed418dff785bbf7a049aacd30cf6198f6e171ac
Noah Sanchez <alu0101134956@ull.edu.es>
2022-06-15 22:53:21 +0100

    Eliminando dict

--- dataLesiones/main.py
+++ dataLesiones/main.py
@@ -21,59 +21,6 @@ getplot.plotNumLesionesPorEquipo(jugadores)
 getplot.plotNumLesionesPorEquipoAgrupadas(jugadores)
 
 # %%
-lesiones = {
-  'Pie': {
-    'Cantidad': 0,
-    'Tobillo': 0,
-    'Talón': 0
-  },
-  'Pierna': {
-    'Cantidad': 0,
-    'Rodilla': 0,
-    'Gemelos': 0,
-    'Peroneos': 0,
-    'Sóleo': 0,
-    'Peroné': 0,
-    'Tibia': 0
-  },
-  'Muslo': {
-    'Cantidad': 0,
-    'Cuádriceps': 0,
-    'Isquiosurales': 0,
-    'Adductores': 0
-  },
-  'Hombro': {
-    'Cantidad': 0,
-    'Clavícula': 0,
-    'Hombro': 0
-  },
-  'Cadera': {
-    'Cantidad': 0,
-    'Psoas': 0,
-    'Ingle': 0,
-    'Glúteo': 0
-  },
-  'Mano': {
-    'Cantidad': 0,
-    'Muñeca': 0,
-    'Dedos': 0
-  },
-  'Tronco': {
-    'Cantidad': 0,
-    'Lumbar': 0
-  },
-  'Antebrazo': {
-    'Cantidad': 0,
-    'Muñeca': 0
-  },
-  'Tórax': {
-    'Cantidad': 0,
-    'Costillas': 0
-  }
-}
-
-
-# %%
 lesiones_previas = getdata.getInjuriesTable(jugadores)
 lesiones_previas
 
